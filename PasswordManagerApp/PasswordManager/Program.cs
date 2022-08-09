5f975b5d442b7342947fad5b45fe9abec1e366e2
skillmaker <anas.chahid777@gmail.com>
2022-08-09 22:17:59 +0100

    added logging using serilog

--- PasswordManagerApp/PasswordManager/Program.cs
+++ PasswordManagerApp/PasswordManager/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//creating a logger from configuration
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .CreateLogger();
+
+//Removing default logging providers
+builder.Logging.ClearProviders();
+
+//Adding serilog as a logger
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
@@ -22,4 +35,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    logger.Information("Application is starting");
+    app.Run();
+    
+}
+catch (Exception ex)
+{
+    logger.Fatal(ex, "Application has not started properly");
+}
