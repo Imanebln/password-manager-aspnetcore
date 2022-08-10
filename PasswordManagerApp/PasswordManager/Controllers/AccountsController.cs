e8b7d3f10063bc3b281e13c27c3cfffd02ea4fb4
skillmaker <anas.chahid777@gmail.com>
2022-08-10 18:35:02 +0100

    added signup endpoint

---
+++ PasswordManagerApp/PasswordManager/Controllers/AccountsController.cs
@@ -0,0 +1,43 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountsController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(UserDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = new ApplicationUser
+                {
+                    UserName = userDto.Username,
+                    Email = userDto.Email
+                };
+
+                var result = await _userManager.CreateAsync(appUser, userDto.Password);
+                if (result.Succeeded)
+                    return Ok("User Created Successfully");
+                else
+                {
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
