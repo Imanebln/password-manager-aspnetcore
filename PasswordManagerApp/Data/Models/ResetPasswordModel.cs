1d1967e8fde85559fd36c6e0568799469b98ec98
skillmaker <anas.chahid777@gmail.com>
2022-08-14 22:45:26 +0100

    Added password request functionality

---
+++ PasswordManagerApp/Data/Models/ResetPasswordModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class ResetPasswordModel
+    {
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword",ErrorMessage = "Passwords do not match!")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
