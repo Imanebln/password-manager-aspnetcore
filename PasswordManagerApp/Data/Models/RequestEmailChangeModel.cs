006217cc834feea63436043ea72b82c9215a95f6
skillmaker <anas.chahid777@gmail.com>
2022-08-22 19:19:20 +0100

    added email change functionality

---
+++ PasswordManagerApp/Data/Models/RequestEmailChangeModel.cs
@@ -0,0 +1,17 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class RequestEmailChangeModel
+    {
+        [Email]
+        public string Email { get; set; } = string.Empty;
+        [Email]
+        public string NewEmail { get; set; } = String.Empty;
+    }
+}
