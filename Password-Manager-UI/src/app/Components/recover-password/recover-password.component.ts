a9a172f9451dfa7a919ef8d57713a817663fd376
Imanebln <boulouane.imane@gmail.com>
2022-09-13 00:11:27 +0100

    updated verification page

--- Password-Manager-UI/src/app/Components/recover-password/recover-password.component.ts
+++ Password-Manager-UI/src/app/Components/recover-password/recover-password.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
@@ -10,30 +10,37 @@ import { AuthService } from 'src/app/Services/auth.service';
   styleUrls: ['./recover-password.component.css']
 })
 export class RecoverPasswordComponent implements OnInit {
+  @Output() redirect:EventEmitter<any> = new EventEmitter();
 
   recoverPassForm! : FormGroup;
+  data!: any;
   constructor(
     private router : Router,
-    private authservice: AuthService
+    private authservice: AuthService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.recoverPassForm = new FormGroup({
       email: new FormControl(null,[Validators.required])
     });
+  
   }
 
   onSubmit(){
     this.authservice.requestPassReset(this.recoverPassForm.value).subscribe({
       next: (res: any) => {
         console.log(res);
-        this.router.navigate(['verification']);
+        this.data = {... this.recoverPassForm};
+        this.redirect.emit(this.data);
+        this.router.navigate(['verification',this.data]);
         // success alert goes here 
       },
       error: (err: HttpErrorResponse) => {
         console.log(err);
         if(err.status == 200){
-          this.router.navigate(['verification']);
+          this.data = {... this.recoverPassForm.value};
+          this.router.navigate(['verification',this.data]);
         }
         // error alert goes here
       }
--- Password-Manager-UI/src/app/Components/verification-link/verification-link.component.ts
+++ Password-Manager-UI/src/app/Components/verification-link/verification-link.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificationLinkComponent implements OnInit {
 
-  constructor() { }
+  data!: any;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.data = this.route.snapshot.params;
+    console.log(this.data);
+    
   }
 
 }
