0f94aae3bbefe6a839ecef0d8a2e150397146254
Anas Chahid <anas.chahid777@gmail.com>
2022-09-14 01:25:28 +0100

    Add username to Token response.

--- PasswordManagerApp/AuthenticationService/Models/AccessTokenModel.cs
+++ PasswordManagerApp/AuthenticationService/Models/AccessTokenModel.cs
@@ -8,17 +8,19 @@ namespace AuthenticationService.Models
 {
     public class AccessTokenModel
     {
-        public AccessTokenModel(string accessToken, DateTime creationDate, DateTime expirationDate, IEnumerable<string> roles)
+        public AccessTokenModel(string accessToken, DateTime creationDate, DateTime expirationDate, IEnumerable<string> roles,string username)
         {
             AccessToken = accessToken;
             CreationDate = creationDate;
             ExpirationDate = expirationDate;
             Roles = roles;
+            Username = username;
         }
 
         public string AccessToken { get; set; } = string.Empty;
         public DateTime CreationDate { get; set; }
         public DateTime ExpirationDate { get; set; }
+        public string Username { get; set; }
         public IEnumerable<string> Roles { get; set; }
     }
 }
