1686db556cee4369ed5e7514c30274d59ab74367
Imanebln <boulouane.imane@gmail.com>
2022-09-25 15:43:42 +0100

    adding generatePdfSummary route to front

--- Password-Manager-UI/src/app/Components/dashboard/dashboard.component.ts
+++ Password-Manager-UI/src/app/Components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { UserService } from './../../Services/user.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   user: any;
+  passwordsList: any;
 
   constructor(
     private router: Router,
@@ -17,7 +19,35 @@ export class DashboardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    
+    this.getCurrentUserData();
+  }
+
+  //generate pdf summary
+  generatePdfSummary(){
+    this.userService.generatePdfSummary().subscribe({
+      next: (res:any) => {
+        console.log(res);
+        
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        
+      }
+    })
+  }
+
+  // get all user passwords
+  getCurrentUserData(){
+    this.userService.getCurrrentUserData().subscribe({
+      next: (res: any) => {
+        this.passwordsList = res;
+        console.log(this.passwordsList);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+
+    })
   }
 
   
