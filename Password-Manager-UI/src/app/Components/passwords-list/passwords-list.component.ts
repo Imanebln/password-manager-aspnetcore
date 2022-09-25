7573b0d8421b06c4c66dadbbb653c43d5a7062c6
Imanebln <boulouane.imane@gmail.com>
2022-09-25 15:42:58 +0100

    updated addNewPassword form

--- Password-Manager-UI/src/app/Components/passwords-list/passwords-list.component.ts
+++ Password-Manager-UI/src/app/Components/passwords-list/passwords-list.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { User } from './../../Models/User';
 import { Component, OnInit } from '@angular/core';
@@ -10,24 +11,33 @@ import { UserService } from 'src/app/Services/user.service';
 })
 export class PasswordsListComponent implements OnInit {
 
+  newPasswordForm!: FormGroup;
   passwordsList: any;
+  newPasswordModel: any;
+  showPassword: boolean = false;
+
   constructor(
     private userservice: UserService
   ) { }
 
   ngOnInit(): void {
     this.getCurrentUserData();
+    this.newPasswordForm = new FormGroup({
+      email: new FormControl(null, [Validators.required]),
+      encryptedPassword: new FormControl(null, [Validators.required]),
+      name: new FormControl(null, [Validators.required])
+    });
   }
 
-  add(){
-    
+  showHidePassword() {
+    this.showPassword = !this.showPassword;
   }
 
+  // get all user passwords
   getCurrentUserData(){
     this.userservice.getCurrrentUserData().subscribe({
       next: (res: any) => {
-        this.passwordsList = res.accountInfos;
-        console.log(res.accountInfos);
+        this.passwordsList = res;
         console.log(this.passwordsList);
       },
       error: (err: HttpErrorResponse) => {
@@ -35,6 +45,20 @@ export class PasswordsListComponent implements OnInit {
       }
 
     })
+  }
+
+  // add new password to user data
+  newPassword(){
+    this.newPasswordModel = this.newPasswordForm.value;
+    this.userservice.newPassword(this.newPasswordModel).subscribe({
+      next: (res:any) => {
+        console.log(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+    })
+    
   }
 
 }
